3af960b23e3142fab59db1f7c043ccebf898a863
Rafi Izdihaar <93192755+rafiizdhr@users.noreply.github.com>
2026-06-06 07:06:17 +0800

    Add files via upload

---
+++ advanced sql/15. Stored Procedures in MySQL.sql
@@ -0,0 +1,50 @@
+-- Stored Procedures
+
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+-- use with stored procedures
+CREATE PROCEDURE large_salaries()
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+CALL large_salaries();
+
+-- in this case, it'll just make procedure large_salaries2() with just one query and the query below just a random select query. the procedure only having one query
+-- which in this case we want to have 2 query in one procedure.
+CREATE PROCEDURE large_salaries2()
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+SELECT * 
+FROM employee_salary
+WHERE salary >= 10000;
+
+-- here is the solution
+
+DELIMITER $$ 
+CREATE PROCEDURE large_salaries3()
+BEGIN
+	SELECT *
+	FROM employee_salary
+	WHERE salary >= 50000;
+	SELECT * 
+	FROM employee_salary
+	WHERE salary >= 10000;
+END $$
+DELIMITER ; 
+
+CALL large_salaries3();
+
+CALL new_procedure();
+
+-- stored procedure with parameter
+CREATE PROCEDURE procedure_param(p_employee_id INT)
+SELECT *
+FROM employee_demographics dem
+WHERE dem.employee_id = p_employee_id
+;
+
+CALL procedure_param(1);
