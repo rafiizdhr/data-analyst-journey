e0820a3dc4f3f1f7abc657bc1fea290b120b5b94
Rafi Izdihaar <rafiizdihar9@gmail.com>
2026-08-02 06:45:37 +0800

    Add online retail RFM analysis project files

--- PORTFOLIO/online-retail-rfm-analysis/sql/02_cleaning.sql
+++ PORTFOLIO/online-retail-rfm-analysis/sql/02_cleaning.sql
@@ -1,21 +1,9 @@
--- Check nulls
-SELECT 
-    SUM(CASE WHEN 'Customer ID' IS NULL THEN 1 ELSE 0 END) AS null_customer,
-    SUM(CASE WHEN Description IS NULL THEN 1 ELSE 0 END) AS null_description
-FROM raw_transactions;
-
--- Identify cancellations (Invoice starts with 'C'
-SELECT COUNT(*) FROM raw_transactions WHERE Invoice LIKE 'C%';
-
--- Create cleaned table: remove cancellations, nulls in CustomerID, negative/zero quantity or price
+-- after checking nulls, dup, canellation transaction, negative quantity or price, 
+-- this part make new one table for cleaned transaction
 CREATE TABLE cleaned_transactions AS
-SELECT * 
+SELECT DISTINCT *
 FROM raw_transactions
-WHERE 'Customer ID' IS NOT NULL
+WHERE "Customer ID" IS NOT NULL
     AND Invoice NOT LIKE 'C%'
-    AND Quantity > 0 
+    AND Quantity > 0
     AND Price > 0;
-
--- Add computed column: line revenue
-ALTER TABLE cleaned_transactions ADD COLUMN LineRevenue REAL;
-UPDATE cleaned_transactions SET LineRevenue = Quantity * Price;
